4c3cf277281237b57d544a937c4d571958bc429c
beatrixbee <seriousman2244@gmail.com>
2023-02-03 00:52:01 +0500

    Fonts&Colors, First Component
    Should add fonts!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,6 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
     </>;
 }
