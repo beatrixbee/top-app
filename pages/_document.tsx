175abfb5683d215788673c1a6e9a06a71a11e327
beatrixbee <seriousman2244@gmail.com>
2023-02-06 02:00:09 +0500

    Added SVG Component and ClassNames Package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document{
     render(): JSX.Element {
         return (
             <Html lang="ru">
-                <Head/>
+                <Head />
                 <body>
                     <Main/>
                     <NextScript/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from '@/components';
+import { Htag, Button } from '@/components';
 
 export default function Home(): JSX.Element {
-    return (
-        <div>
-            <Htag tag='h1'>Text</Htag>
-        </div>
-    );
+	return (
+		<div>
+			<Htag tag='h1'>Text</Htag>
+            <Button appearance='primary' className='asdasd' arrow='right'>Button</Button>
+            <Button appearance='ghost'>Button</Button>
+		</div>
+	);
 }
