b6894afbbf0ffbc75e96136a36f8980576ea0ac1
beatrixbee <seriousman2244@gmail.com>
2023-02-06 14:06:32 +0500

    useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Htag, Button, Ptag, Tag } from '@/components';
+import { useState } from 'react';
 
 export default function Home(): JSX.Element {
+	const [counter, setCounter] = useState<number>(0);
 	return (
 		<div>
-			<Htag tag='h1'>Text</Htag>
-            <Button appearance='primary' className='asdasd' arrow='right'>Button</Button>
-            <Button appearance='ghost'>Button</Button>
+			<Htag tag='h1'>{counter}</Htag>
+			<Button appearance='primary' className='asdasd' arrow='right' onClick={() => setCounter(x => x+1)}>Button</Button>
+			<Button appearance='ghost'>Button</Button>
 			<Ptag className='ppp'>Some normal text</Ptag>
 			<Ptag className='ppp' size='s'>Some small text</Ptag>
 			<Ptag className='ppp' size='l'>Some large text</Ptag>
