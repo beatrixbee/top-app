068af10cfbccf2b025b5d2fac77e640ab49329f9
beatrixbee <seriousman2244@gmail.com>
2023-02-12 17:24:45 +0500

    Product Component Grid

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,13 +1,43 @@
 import { ProductProps } from './Product.props';
 import cn from 'classnames';
 import styles from './Product.module.css';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
+import { Button } from '../Button/Button';
 
 
 export const Product = ({ product, className, ...props}: ProductProps): JSX.Element => {
 
 	return (
-		<div className={cn(className, styles.product)} {...props}>
-			{product.title}
-		</div>
+		<Card className={styles.product}>
+			<div className={styles.logo} ><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image } alt={product.title} /></div>
+			<div className={styles.title} >{product.title}</div>
+			<div className={styles.price} >{product.price}</div>
+			<div className={styles.credit} >{product.credit}</div>
+			<div className={styles.rating} ><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
+			<div className={styles.tags} >{product.categories.map(c => <Tag key={c} color='ghost'>{c}</Tag>)}</div>
+			<div className={styles.priceTitle}>цена</div>
+			<div className={styles.creditTitle}>кредит</div>
+			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<hr className={styles.hr}/>
+			<div className={styles.description}>{product.description}</div>
+			<div className={styles.features}>Features</div>
+			<div className={styles.advBlock}>
+				<div className={styles.advantages}>
+					<div>Advantages</div>
+					<div>{product.advantages}</div>
+				</div>
+				<div className={styles.disadvantages}>
+					<div>Disadvantages</div>
+					<div>{product.disadvantages}</div>
+				</div>
+			</div>
+			<hr className={styles.hr}/>
+			<div className={styles.actions}>
+				<Button appearance='primary'>Узнать подробнее</Button>
+				<Button appearance='ghost'>Читать отзывы</Button>
+			</div>
+		</Card>
 	);
 };
