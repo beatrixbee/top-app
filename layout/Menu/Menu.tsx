6d4c4ba3788b312842f1db657e8f068e1f23bf84
beatrixbee <seriousman2244@gmail.com>
2023-05-10 19:40:56 +0800

    Added landmarks and role

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -138,8 +138,8 @@ export const Menu = (): JSX.Element => {
 	};
 
 	return (
-		<div className={styles.menu}>
+		<nav className={styles.menu} role="navigation">
 			{buildFirstLevel()}
-		</div>
+		</nav>
 	);
 };
